6647016a50841cdd53825dffd0eb97d03e8b6313
Saotomefirst <jorge.appleseed@gmail.com>
2022-01-02 20:29:20 -0300

    Implementando Local Notification
    Implementado o funcionamento das Notificações locais, disparadas diretamente de dentro do app para o sistema operacional

--- app/src/main/java/com/saotome/applicationnotification/MainActivity.kt
+++ app/src/main/java/com/saotome/applicationnotification/MainActivity.kt
@@ -2,10 +2,20 @@ package com.saotome.applicationnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var enviarNotificacaoBotao: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        enviarNotificacaoBotao = findViewById(R.id.enviar_notificacao_botao)
+
+        enviarNotificacaoBotao.setOnClickListener {
+            this.showNotification("1234", "Bootcamp Android", "Kotlin Android Curso")
+        }
     }
 }
